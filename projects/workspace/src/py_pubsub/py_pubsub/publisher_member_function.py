94025719981f480714a264159ed2ca0443516ef5
lonebots <jishnusurajila@gmail.com>
2022-09-21 05:53:44 +0530

    update --data-type-change -publisher_member_function.py

--- projects/workspace/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ projects/workspace/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -1,23 +1,23 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Num  # CHANGE
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        self.publisher_ = self.create_publisher(Num, 'topic', 10)  # CHANGE
+        timer_period = 0.5
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Num()  # CHANGE
+        msg.num = self.i  # CHANGE
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%d"' % msg.num)  # CHANGE
         self.i += 1
 
 
@@ -28,9 +28,6 @@ def main(args=None):
 
     rclpy.spin(minimal_publisher)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
