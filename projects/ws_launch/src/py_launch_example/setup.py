2f7d1d061a6768efd5f7047d3b1279ee397253af
lonebots <jishnusurajila@gmail.com>
2022-12-28 21:59:59 +0530

    fix: updated setup.py file to recognise the launch folder

--- projects/ws_launch/src/py_launch_example/setup.py
+++ projects/ws_launch/src/py_launch_example/setup.py
@@ -1,25 +1,14 @@
+import os
+from glob import glob
 from setuptools import setup
 
-package_name = 'py_launch_example'
+package_name = "py_launch_example"
 
 setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    # Other parameters ...
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='jishnu',
-    maintainer_email='jishnusurajila@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+        # ... Other data files
+        # Include all launch files.
+        (os.path.join("share", package_name), glob("launch/*launch.[pxy][yma]*"))
+    ]
 )
