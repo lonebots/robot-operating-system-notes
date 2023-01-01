89066b332efad37e9e8656787282aa8a94647954
lonebots <jishnusurajila@gmail.com>
2023-01-01 13:58:46 +0530

    fix:code refactoring, deleted old folder and workspaces

--- workspace/ros2_workspace/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jishnu/project/ros2-project/projects/ros2_workspace/src/ros_tutorials/turtlesim
-# Build directory: /home/jishnu/project/ros2-project/projects/ros2_workspace/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
