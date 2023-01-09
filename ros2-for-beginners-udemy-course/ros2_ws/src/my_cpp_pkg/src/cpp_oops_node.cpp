d1df522e577c7b1ed4a81d1686cc2ef88c7b3171
lonebots <jishnusurajila@gmail.com>
2023-01-09 17:34:01 +0530

    [ros2-course] oops node counter added

--- ros2-for-beginners-udemy-course/ros2_ws/src/my_cpp_pkg/src/cpp_oops_node.cpp
+++ ros2-for-beginners-udemy-course/ros2_ws/src/my_cpp_pkg/src/cpp_oops_node.cpp
@@ -2,11 +2,26 @@
 
 class MyNode : public rclcpp::Node{
     public:
-        MyNode() : Node("cpp_oops_test"){
+        MyNode() : Node("cpp_oops_test"), counter_(0)
+        {
             RCLCPP_INFO(this->get_logger(),"Hello from C++ oops Node!");
+
+            // initialize the timer
+            timer_ = this->create_wall_timer(std::chrono::seconds(1),
+            std::bind(&MyNode::timerCallback, this));  // function binding to the class instance using 'this' keyword
         }
 
     private :
+
+    // create timer callback to implement a counter 
+    void timerCallback (){
+        counter_ ++;
+        RCLCPP_INFO(this->get_logger(),"Hello count value : %d",counter_);
+    }
+
+    // create the timer 
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
 };
 
 int main(int argc , char **argv) {
