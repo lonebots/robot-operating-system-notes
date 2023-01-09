6ff4ba71c1d1e18541704ef8813ec9505af079be
lonebots <jishnusurajila@gmail.com>
2023-01-09 16:17:18 +0530

    [ros2-course]: cpp oops node program complete

--- ros2-for-beginners-udemy-course/ros2_ws/src/my_cpp_pkg/src/cpp_oops_node.cpp
+++ ros2-for-beginners-udemy-course/ros2_ws/src/my_cpp_pkg/src/cpp_oops_node.cpp
@@ -1,11 +1,18 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node{
+    public:
+        MyNode() : Node("cpp_oops_test"){
+            RCLCPP_INFO(this->get_logger(),"Hello from C++ oops Node!");
+        }
+
+    private :
+};
+
 int main(int argc , char **argv) {
     rclcpp::init(argc,argv);
 
-    auto node = std::make_shared<rclcpp::Node>("cpp_opps_test");
-
-    RCLCPP_INFO(node->get_logger(),"Hello from C++ oops Node!");
+    auto node = std::make_shared<MyNode>( );
 
     rclcpp::spin(node);
 
