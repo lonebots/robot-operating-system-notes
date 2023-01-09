0af421b331a5c7f5c64cb930d63042f4ce2b909c
lonebots <jishnusurajila@gmail.com>
2023-01-09 16:09:55 +0530

    [ros2-course]: fix : typo fixed in oops cpp program

--- ros2-for-beginners-udemy-course/ros2_ws/src/my_cpp_pkg/src/cpp_oops_node.cpp
+++ ros2-for-beginners-udemy-course/ros2_ws/src/my_cpp_pkg/src/cpp_oops_node.cpp
@@ -3,7 +3,7 @@
 int main(int argc , char **argv) {
     rclcpp::init(argc,argv);
 
-    auto node = std:make_shared<rclcpp::Node>("cpp_opps_test");
+    auto node = std::make_shared<rclcpp::Node>("cpp_opps_test");
 
     RCLCPP_INFO(node->get_logger(),"Hello from C++ oops Node!");
 
