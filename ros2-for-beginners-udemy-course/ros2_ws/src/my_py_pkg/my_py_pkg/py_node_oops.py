bbd42ac0552cc84dbfad708fa3b60f13e8a37ebc
lonebots <jishnusurajila@gmail.com>
2023-01-09 13:59:45 +0530

    node: implemented python node with class

--- ros2-for-beginners-udemy-course/ros2_ws/src/my_py_pkg/my_py_pkg/py_node_oops.py
+++ ros2-for-beginners-udemy-course/ros2_ws/src/my_py_pkg/my_py_pkg/py_node_oops.py
@@ -3,11 +3,18 @@
 import rclpy
 from rclpy.node import Node
 
+# create a class inheriting from the 'Node' object
+class MyNode(Node) :
+
+    # initialise and call the super class with name of the node
+    def __init__(self) :
+        super().__init__("py_oops_node")
+        self.get_logger().info("hello from python oops node!")
+    
 
 def main(args=None):
     rclpy.init()
-    node = Node("py_oop_node")
-    node.get_logger().info("hello from python oops node!")
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
