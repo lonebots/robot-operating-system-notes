038c9d947e5e81c6ddf8d728041b5ca9ae564e96
lonebots <jishnusurajila@gmail.com>
2023-01-09 14:17:24 +0530

    chore: build success for counter implementation in oops node

--- ros2-for-beginners-udemy-course/ros2_ws/src/install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/py_node_oops.py
+++ ros2-for-beginners-udemy-course/ros2_ws/src/install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/py_node_oops.py
@@ -4,13 +4,25 @@ import rclpy
 from rclpy.node import Node
 
 # create a class inheriting from the 'Node' object
-class MyNode(Node) :
+
+
+class MyNode(Node):
 
     # initialise and call the super class with name of the node
-    def __init__(self) :
+    def __init__(self):
         super().__init__("py_oops_node")
         self.get_logger().info("hello from python oops node!")
-    
+
+        # implement a timer that counts
+        self.counter_ = 0
+        # calls timer_callback every 1 sec
+        self.create_timer(1, self.timer_callback)
+
+    # timer callback function
+    def timer_callback(self):
+        self.counter_ += 1  # increment by 1 every second
+        self.get_logger().info("oops node counter value : " + str(self.counter_))
+
 
 def main(args=None):
     rclpy.init()
