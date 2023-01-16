fb8121642dee75cf72f3d9b0a49708aebec4fb58
lonebots <jishnusurajila@gmail.com>
2023-01-16 13:24:28 +0530

    chore: smarphone node executable added in setup.py

--- ros2-for-beginners/ros2_ws/src/my_py_pkg/setup.py
+++ ros2-for-beginners/ros2_ws/src/my_py_pkg/setup.py
@@ -22,12 +22,8 @@ setup(
         'console_scripts': [
             "my_node = my_py_pkg.my_first_node:main",
             "my_oops_node = my_py_pkg.py_node_oops:main",
-<<<<<<< HEAD
             "robot_news_station = my_py_pkg.robot_news_station:main",
             "smartphone = my_py_pkg.smartphone:main"
-=======
-            "robot_news_station = my_py_pkg.robot_news_station:main"
->>>>>>> 874f3ef (setup: added executable to the robot news publisher)
         ],
     },
 )
