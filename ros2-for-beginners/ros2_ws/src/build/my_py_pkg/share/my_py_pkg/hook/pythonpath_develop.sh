c40be7160e4932c6240caea8db4a794c1b150b29
lonebots <jishnusurajila@gmail.com>
2023-01-17 23:48:09 +0530

    chore: install,log,build added to git ignore

--- ros2-for-beginners/ros2_ws/src/build/my_py_pkg/share/my_py_pkg/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/jishnu/robotics/ros-git/ros2-for-beginners/ros2_ws/src/build/my_py_pkg"
