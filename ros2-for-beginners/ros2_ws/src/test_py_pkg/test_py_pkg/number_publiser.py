5b521ab495199e745473758c827e4d8ecee7f70b
lonebots <jishnusurajila@gmail.com>
2023-01-18 00:38:21 +0530

    fix: build corrected for activity 2 python implementation

--- ros2-for-beginners/ros2_ws/src/test_py_pkg/test_py_pkg/number_publiser.py
+++ ros2-for-beginners/ros2_ws/src/test_py_pkg/test_py_pkg/number_publiser.py
@@ -2,19 +2,20 @@
 
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import int
+from example_interfaces.msg import Int64
 
 
 class NumberPubliserNode(Node):
     def __init__(self) -> None:
         super().__init__("number_publisher")
-
-        self.publisher_ = self.create_publisher(int, "number", 10)
+        self.number_ = 10
+        self.number_publisher_ = self.create_publisher(Int64, "number", 10)
+        self.timer_ = self.create_timer(1, self.publishNumber)
 
     def publishNumber(self):
-        msg = int()
-        msg.data = 123
-        self.publisher_.publish(msg)
+        msg = Int64()
+        msg.data = self.number_
+        self.number_publisher_.publish(msg)
 
 
 def main(args=None):
