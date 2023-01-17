8fd8a3fa612bf3b80f9b990bff4dd3af7e4484c3
Jishnu <jishnusurajila@gmail.com>
2023-01-17 12:10:25 +0530

    Cpp publisher subscriber (#24)
    * node: cpp publisher node complete
    
    * dep: added install executable target in package.xml and cmake file
    
    * chore: added smarphone subscriber to cmake file
    
    * node: cpp subscriber node complete
    
    * docs: Topic debugging with cli tools added
    
    * docs: topic remaping notes added

---
+++ ros2-for-beginners/ros2_ws/src/my_cpp_pkg/src/smartphone.cpp
@@ -0,0 +1,44 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+class SmartPhoneNode : public rclcpp::Node
+{
+
+public:
+    SmartPhoneNode() : Node("smartphone")
+    {
+
+        /*
+        initialize subscriber
+            the function takes in 3 parameters they are
+                1. topic name
+                2. queue size
+                3. callback --> additional code required since the callback function takes in parameters.
+        */
+        subscriber_ = this->create_subscription<example_interfaces::msg::String>("robot_news", 10,
+                                                                                 std::bind(&SmartPhoneNode::callbackRobotNews, this, std::placeholders::_1));
+        RCLCPP_INFO(this->get_logger(), "cpp smartphone node has been started")
+    }
+
+private:
+    // call back for handling subscription
+    void callbackRobotNews(const example_interfaces::msg::String::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "%s", msg->data.c_str());
+    }
+
+    // declaration
+    rclcpp::Subcription<example_interfaces::msg::String>::SharedPtr subscriber_;
+
+}
+
+int
+main(int argc, char **argv)
+{
+    rclcpp::init();
+    auto node = std::make_shared<SmartPhoneNode>();
+    rclcpp::spin(node);
+rclcpp:
+    shutdown();
+    return 0;
+}
