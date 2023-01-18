1315dc969e70662f51c580e33519fa0ae04943db
lonebots <jishnusurajila@gmail.com>
2023-01-18 14:21:10 +0530

    node:service server node added

--- ros2-for-beginners/ros2_ws/src/my_py_pkg/setup.py
+++ ros2-for-beginners/ros2_ws/src/my_py_pkg/setup.py
@@ -23,7 +23,8 @@ setup(
             "my_node = my_py_pkg.my_first_node:main",
             "my_oops_node = my_py_pkg.py_node_oops:main",
             "robot_news_station = my_py_pkg.robot_news_station:main",
-            "smartphone = my_py_pkg.smartphone:main"
+            "smartphone = my_py_pkg.smartphone:main",
+            "add_two_ints_server = my_py_pkg.add_two_ints_server:main"
         ],
     },
 )
