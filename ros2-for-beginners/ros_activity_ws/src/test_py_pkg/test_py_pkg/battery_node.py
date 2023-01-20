2b0a2e0956869187c6be1429b8d4e025a4139a05
lonebots <jishnusurajila@gmail.com>
2023-01-21 00:13:38 +0530

    feat:battery node added

--- ros2-for-beginners/ros_activity_ws/src/test_py_pkg/test_py_pkg/battery_node.py
+++ ros2-for-beginners/ros_activity_ws/src/test_py_pkg/test_py_pkg/battery_node.py
@@ -2,11 +2,47 @@
 
 import rclpy
 from rclpy.node import Node
+from functools import partial
+
+from custom_interfaces.srv import SetLed
 
 
 class BatteryNode (Node):
     def __init__(self) -> None:
         super().__init__("battery_node")
+        self.battery_status_update_client = self.create_client(
+            SetLed, 'set_led')
+
+        self.battery_empty_timer_ = self.create_timer(
+            4, self.call_set_battery_empty)
+        self.battery_full_timer_ = self.create_timer(
+            6, self.call_set_battery_full)
+
+        self.get_logger().info("battery node started")
+
+    def call_set_battery_full(self):
+        request = SetLed.Request()
+        request.led_number = 3
+        request.state = 'on'
+        future = self.battery_status_update_client.call_async(request)
+
+        future.add_done_callback(partial(self.callback_set_battery))
+
+    def call_set_battery_empty(self):
+        request = SetLed.Request()
+        request.led_number = 3
+        request.state = 'off'
+        future = self.battery_status_update_client.call_async(request)
+
+        future.add_done_callback(partial(self.callback_set_battery))
+
+    def callback_set_battery(self, future):
+        try:
+            response = future.result()
+            self.get_logger().info(f"{response}")
+        except Exception as e:
+            self.get_logger().info(
+                f"service call failure with exception : {e}")
 
 
 def main(args=None):
