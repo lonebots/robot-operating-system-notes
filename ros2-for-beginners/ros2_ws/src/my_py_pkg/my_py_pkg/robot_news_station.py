cfb6a97a6607aa1830c82ba60e973b9fff20d3c6
lonebots <jishnusurajila@gmail.com>
2023-01-22 23:51:32 +0530

    fix: name and frequency parameter added

--- ros2-for-beginners/ros2_ws/src/my_py_pkg/my_py_pkg/robot_news_station.py
+++ ros2-for-beginners/ros2_ws/src/my_py_pkg/my_py_pkg/robot_news_station.py
@@ -10,6 +10,15 @@ class RobotNewsStationNode(Node):
     def __init__(self) -> None:
         super().__init__("robot_news_station")
 
+        # parameters
+        self.declare_parameter('robot_name', 'test_robot_1')
+        self.declare_parameter('publishing_frequency', 1.0)
+
+        # assgin parameter to variables
+        self.robot_name_ = self.get_parameter('robot_name').value
+        self.publishing_frequency_ = self.get_parameter(
+            'publishing_frequency').value
+
         '''
         create a publisher object
         using create_publisher method of Node class 
@@ -22,7 +31,8 @@ class RobotNewsStationNode(Node):
         self.publisher_ = self.create_publisher(String, "robot_news", 10)
 
         # create a timer to publish the message in regular intervals
-        self.timer_ = self.create_timer(0.5, self.publish_news)
+        self.timer_ = self.create_timer(
+            1.0/self.publishing_frequency_, self.publish_news)
 
         # log
         self.get_logger().info("Robot News Station has been started.")
@@ -30,7 +40,7 @@ class RobotNewsStationNode(Node):
     # create a function to publish the news
     def publish_news(self):
         msg = String()
-        msg.data = 'Hello'
+        msg.data = 'Hello from ' + self.robot_name_ + " via robot news station!"
         self.publisher_.publish(msg)
 
 
