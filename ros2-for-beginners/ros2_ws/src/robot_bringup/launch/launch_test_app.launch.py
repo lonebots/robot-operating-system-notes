800e8fe69cc55b4d5e283d58c90de9859e8f38e6
lonebots <jishnusurajila@gmail.com>
2023-01-22 23:27:09 +0530

    feat:lauch files added

---
+++ ros2-for-beginners/ros2_ws/src/robot_bringup/launch/launch_test_app.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # python_oop_node
+    python_oops_node = Node(
+        package="my_py_pkg",
+        executable="my_oops_node",
+        name="my_python_oops_node"
+    )
+
+    # robot_news station
+    robot_news_station = Node(
+        package="my_cpp_pkg",
+        executable="robot_news_station",
+        name="yoyo_news_robot",
+        remappings=[
+            ('robot_news', 'yoyo_robot_news_topic')  # for topic and services
+
+        ],
+        parameters=[
+            #  {key: value } for parameters
+        ]
+    )
+
+    ld.add_action(python_oops_node)
+    ld.add_action(robot_news_station)
+
+    return ld
