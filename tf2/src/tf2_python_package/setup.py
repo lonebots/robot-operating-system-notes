bf83f3f53c01659248cc384e483140de6c3e7d60
lonebots <jishnusurajila@gmail.com>
2023-04-07 16:24:49 +0530

    tf2: static transform broadcaster implementation added

--- tf2/src/tf2_python_package/setup.py
+++ tf2/src/tf2_python_package/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "static_turtle_tf2_broadcaster = tf2_python_package.stf_publisher:main",
         ],
     },
 )
