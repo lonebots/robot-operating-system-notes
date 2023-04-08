3c625dc1f447d30465bc606bcad396125ec26ad6
lonebots <jishnusurajila@gmail.com>
2023-04-08 19:53:42 +0530

    tf2: python dynamic transform broadcaster node, launch file added

--- tf2/src/tf2_python_package/setup.py
+++ tf2/src/tf2_python_package/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'tf2_python_package'
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.launch.py'))),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,6 +26,7 @@ setup(
     entry_points={
         'console_scripts': [
             "static_turtle_tf2_broadcaster = tf2_python_package.stf_publisher:main",
+            "turtle_tf2_broadcaster = tf2_python_package.dtf_broadcaster:main",
         ],
     },
 )
