a77dd30a46db564ea8f416c832527195dd8c2a84
lonebots <jishnusurajila@gmail.com>
2023-04-09 00:47:11 +0530

    tf2: tranform listener implementation completed

--- tf2/src/tf2_python_package/launch/turtle_tf2_demo.launch.py
+++ tf2/src/tf2_python_package/launch/turtle_tf2_demo.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
 
 def generate_launch_description():
     return LaunchDescription([
@@ -15,6 +16,27 @@ def generate_launch_description():
             name='broadcaster1',
             parameters=[
                 {'turtlename': 'turtle1'}
+            ]
+        ),
+        DeclareLaunchArgument(
+            'target_frame',
+            default_value='turtle1',
+            description='Target frame name'
+        ),
+        Node(
+            package='tf2_python_package',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        Node(
+            package='tf2_python_package',
+            executable='turtle_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': LaunchConfiguration('target_frame')}
             ]
         ),
     ])
